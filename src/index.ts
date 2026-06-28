ed30f6cf6d7ab1e358918eb6621d7d8309f60c70
maaaashi <ooka.ms.0r7e2p3@gmail.com>
2026-06-28 19:54:15 +0900

    query method init

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import { Hono } from "hono";
+import { queryUsersHandler } from "./handlers/queryUsers";
 
 const app = new Hono();
 
 app.get("/ping", (c) => c.text("pong"));
+
+app.on("query", "/users", queryUsersHandler);
 
 export default app;
