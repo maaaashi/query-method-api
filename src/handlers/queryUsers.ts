4f54299c7c2ab8f6a3136d298fb3c703e9bc3369
maaaashi <ooka.ms.0r7e2p3@gmail.com>
2026-06-29 20:15:53 +0900

    ユーザークエリハンドラーを実装し、ユーザー情報をJSON形式で返すように変更

--- src/handlers/queryUsers.ts
+++ src/handlers/queryUsers.ts
@@ -1,5 +1,20 @@
 import type { Context } from "hono";
 
+class User {
+  constructor(
+    public readonly id: number,
+    public readonly name: string,
+  ) {}
+}
+
+type Users = User[];
+
 export const queryUsersHandler = async (c: Context) => {
-  return c.text("ok");
+  return c.json<Users>([
+    new User(1, "田中 太郎"),
+    new User(2, "佐藤 花子"),
+    new User(3, "鈴木 次郎"),
+    new User(4, "高橋 三郎"),
+    new User(5, "伊藤 四郎"),
+  ]);
 };
