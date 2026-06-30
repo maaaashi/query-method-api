dbc56e04a87138ebada43643a2d73418d8f6b5a0
maaaashi <ooka.ms.0r7e2p3@gmail.com>
2026-06-30 21:31:37 +0900

    415

--- src/handlers/queryUsers.ts
+++ src/handlers/queryUsers.ts
@@ -10,6 +10,12 @@ class User {
 type Users = User[];
 
 export const queryUsersHandler = async (c: Context) => {
+  const contentType = c.req.header("Content-Type") || "";
+
+  if (!contentType.includes("application/json")) {
+    return c.json({ error: "Unsupported Media Type" }, 415);
+  }
+
   return c.json<Users>([
     new User(1, "田中 太郎"),
     new User(2, "佐藤 花子"),
