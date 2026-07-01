7e6a9b2d0843b828533f7196e3851f2747b2061f
maaaashi <ooka.ms.0r7e2p3@gmail.com>
2026-07-01 22:45:11 +0900

    @hono/zod-validatorを追加し、ユーザークエリハンドラーをリファクタリングしてエラーハンドリングを強化

--- src/handlers/queryUsers.ts
+++ src/handlers/queryUsers.ts
@@ -1,4 +1,6 @@
+import { zValidator } from "@hono/zod-validator";
 import type { Context } from "hono";
+import { createFactory } from "hono/factory";
 import * as z from "zod";
 
 class User {
@@ -16,38 +18,25 @@ const RequestJsonSchema = z
   })
   .strict();
 
-export const queryUsersHandler = async (c: Context) => {
-  const contentType = c.req.header("Content-Type") || "";
+const factory = createFactory();
 
-  if (!contentType.includes("application/json")) {
-    return c.json({ error: "Unsupported Media Type" }, 415);
-  }
+export const queryUsersHandlers = factory.createHandlers(
+  zValidator("json", RequestJsonSchema, (result, c) => {
+    if (!result.success) {
+      return c.json({ error: "Invalid request body" }, 400);
+    }
+  }),
+  async (c) => {
+    const requestJson = c.req.valid("json");
 
-  const body = await c.req.text();
+    console.log(requestJson);
 
-  let bodyJson = {};
-
-  try {
-    bodyJson = JSON.parse(body);
-  } catch (e) {
-    console.error(e);
-    return c.json({ error: "Invalid JSON" }, 400);
-  }
-
-  try {
-    RequestJsonSchema.parse(bodyJson);
-  } catch (e) {
-    console.error(e);
-    return c.json({ error: "Invalid request body" }, 400);
-  }
-
-  console.log(bodyJson);
-
-  return c.json<Users>([
-    new User(1, "田中 太郎"),
-    new User(2, "佐藤 花子"),
-    new User(3, "鈴木 次郎"),
-    new User(4, "高橋 三郎"),
-    new User(5, "伊藤 四郎"),
-  ]);
-};
+    return c.json<Users>([
+      new User(1, "田中 太郎"),
+      new User(2, "佐藤 花子"),
+      new User(3, "鈴木 次郎"),
+      new User(4, "高橋 三郎"),
+      new User(5, "伊藤 四郎"),
+    ]);
+  },
+);
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import { Hono } from "hono";
-import { queryUsersHandler } from "./handlers/queryUsers";
+import { queryUsersHandlers } from "./handlers/queryUsers";
 
 const app = new Hono();
 
 app.get("/ping", (c) => c.text("pong"));
 
-app.on("query", "/users", queryUsersHandler);
+app.on("query", "/users", ...queryUsersHandlers);
 
 export default app;
